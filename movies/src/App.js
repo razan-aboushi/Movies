495331e3ade5a773c4c95eafcb5285346db22c61
razan-aboushi <103279516+razan-aboushi@users.noreply.github.com>
2023-05-16 15:02:30 -0700

    Updates files

--- movies/src/App.js
+++ movies/src/App.js
@@ -3,16 +3,13 @@ import axios from "axios";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [genres, setGenres] = useState([]);
-  const [selectedGenre, setSelectedGenre] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [error, setError] = useState(false);
 
   useEffect(() => {
     // Make a request to your Express server endpoint to retrieve the movies list
-    axios
-      .get("http://localhost:4000/movies") // Update the port to match the Express server's port
+    axios.get("http://localhost:4000/movies") // Update the port to match the Express server's port
       .then((response) => {
         setMovies(response.data);
       })
@@ -20,35 +17,16 @@ function App() {
         console.error(error);
         setError(true);
       });
-
-    // Make a request to fetch the list of genres
-    axios
-      .get("http://localhost:4000/genres") // Update the port to match the Express server's port
-      .then((response) => {
-        if (Array.isArray(response.data)) {
-          setGenres(response.data);
-        } else {
-          setError(true);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        setError(true);
-      });
   }, []);
 
-  function handleGenreChange(event) {
-    setSelectedGenre(event.target.value);
-  }
-
+  console.log(movies);
   function handleSearchChange(event) {
     setSearchTerm(event.target.value);
   }
 
   function handleMovieClick(movieId) {
     // Make a request to fetch the details of a specific movie
-    axios
-      .get(`http://localhost:4000/movies/${movieId}`) // Update the port to match the Express server's port
+    axios.get(`http://localhost:4000/movies/${movieId}`) // Update the port to match the Express server's port
       .then((response) => {
         setSelectedMovie(response.data);
       })
@@ -58,55 +36,19 @@ function App() {
   }
 
   function filterMovies(movie) {
-    if (!selectedGenre && !searchTerm) {
+    if (!searchTerm) {
       return true; // Show all movies when no genre or search term is selected
     }
-    if (
-      selectedGenre &&
-      movie.genre_ids.includes(parseInt(selectedGenre))
-    ) {
-      return true;
-    }
-    if (
-      searchTerm &&
-      movie.title.toLowerCase().includes(searchTerm.toLowerCase())
-    ) {
+    
+    if (searchTerm && movie.title.toLowerCase().includes(searchTerm.toLowerCase())) {
       return true;
     }
     return false;
   }
-
-  if (error) {
-    return <div>Error occurred while fetching data.</div>;
-  }
-
+  
   return (
     <div className="container">
       <h1 className="mb-4">Movies</h1>
-      <div className="mb-3">
-        <label htmlFor="genre" className="form-label">
-          Filter by Genre:
-        </label>
-        <select
-          id="genre"
-          className="form-select"
-          value={selectedGenre}
-          onChange={handleGenreChange}
-        >
-          <option value="">All</option>
-          {genres.length > 0 && (
-            <>
-              <option value="">All</option>
-              {genres.map((genre) => (
-                <option key={genre.id} value={genre.id}>
-                  {genre.name}
-                </option>
-              ))}
-            </>
-          )}
-
-        </select>
-      </div>
 
       <div className="mb-3">
         <label htmlFor="search" className="form-label">
@@ -129,14 +71,15 @@ function App() {
               className="list-group-item d-flex justify-content-between align-items-center animate__animated animate__fadeIn"
               onClick={() => handleMovieClick(movie.id)}
             >
-              {movie.title}
-              <span className="badge bg-primary">Details</span>
+              <div className="movie-info">
+                <h4>{movie.title}</h4>
+                <img src={movie.image[0][1]} alt={movie.title} />
+              </div>
             </li>
           ))
         ) : (
           <div>No movies found.</div>
         )}
-
       </ul>
 
       {selectedMovie && (
--- server/apiMovies.js
+++ server/apiMovies.js
@@ -4,62 +4,43 @@ const cors = require('cors');
 
 const app = express();
 const port = 4000;
-
 app.use(cors());
 
+const options = {
+  method: 'GET',
+  url: 'https://imdb-top-100-movies1.p.rapidapi.com/',
+  headers: {
+    'X-RapidAPI-Key': '1b8eccbabbmsh47cbf857f426fb5p1dc6cfjsne1b55dbcb54b',
+    'X-RapidAPI-Host': 'imdb-top-100-movies1.p.rapidapi.com'
+  }
+};
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 // API endpoint to fetch a list of movies
 app.get('/movies', (req, res) => {
-    axios.get('https://api.themoviedb.org/3/movie/550?', {
-        params: {
-            api_key: 'f7c60082eff916bf6b71057813e68f3c',
-        },}).then(response => {
-            const movies = response.data;
-            res.json(movies);
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).json({ error: 'Failed to fetch movies' });
-        });
+  axios.request(options).then(response => {
+    res.json(response.data);
+  }).catch(error => {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch movies' });
+  });
 });
 
-
-
-// API endpoint to fetch a list of genres
-app.get('/genres', (req, res) => {
-    axios.get('https://api.themoviedb.org/3/movie/550?', {
-        params: {
-            api_key: 'f7c60082eff916bf6b71057813e68f3c',
-        },
-    }).then(response => {
-            const genres = response.data;
-            res.json(genres);
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).json({ error: 'Failed to fetch genres' });
-        });
-});
 
 // API endpoint to fetch details of a specific movie
 app.get('/movies/:movieId', (req, res) => {
-    const { movieId } = req.params;
-  
-    axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
-        params: {
-          api_key: 'f7c60082eff916bf6b71057813e68f3c',
-        },
-      })
-      .then(response => {
-        const movie = response.data;
-        res.json(movie);
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to fetch movie details' });
-      });
-  });
-  
+  const { movieId } = req.params;
+
+  axios.get(`https://imdb-top-100-movies1.p.rapidapi.com/movies/${movieId}`, options)
+    .then(response => {
+      const movie = response.data;
+      res.json(movie);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to fetch movie details' });
+    });
+});
